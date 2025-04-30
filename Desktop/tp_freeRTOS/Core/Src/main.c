106421a76e8df98a479dd480c36ba80238d2d6a6
Rayane <rayan.hf@ensea.fr>
2025-04-30 17:18:59 +0200

    réentrance exclusion mutuelle

--- Desktop/tp_freeRTOS/Core/Src/main.c
+++ Desktop/tp_freeRTOS/Core/Src/main.c
@@ -1,48 +1,34 @@
 #include "main.h"
 #include "cmsis_os.h"
-#include "stdio.h"
-#include "queue.h"
+#include <stdio.h>
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h" // À placer après FreeRTOS
 
 UART_HandleTypeDef huart1;
 
-TaskHandle_t taskGiveHandle;
-TaskHandle_t taskTakeHandle;
-TickType_t giveDelay = 100;
-QueueHandle_t myQueue;
+/* ---- Configuration tâches ---- */
+#define STACK_SIZE       256
+#define TASK1_PRIORITY   1
+#define TASK2_PRIORITY   2
+#define TASK1_DELAY      1
+#define TASK2_DELAY      2
 
+/* ---- Handles ---- */
+TaskHandle_t task1Handle;
+TaskHandle_t task2Handle;
+SemaphoreHandle_t printMutex;
+
+/* ---- Prototypes ---- */
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART1_UART_Init(void);
+void task_bug(void *pvParameters);
 
-void TaskGive(void *argument);
-void TaskTake(void *argument);
-
-void TaskGive(void *argument)
-{
-  for (;;)
-  {
-    printf("TaskGive: Envoi de %lu dans la queue\r\n", giveDelay);
-    xQueueSend(myQueue, &giveDelay, portMAX_DELAY);
-    vTaskDelay(pdMS_TO_TICKS(giveDelay));
-    giveDelay += 100;
-  }
-}
-
-void TaskTake(void *argument)
-{
-  TickType_t receivedValue;
-  for (;;)
-  {
-    if (xQueueReceive(myQueue, &receivedValue, pdMS_TO_TICKS(1000)) == pdPASS)
-    {
-      printf("TaskTake: Reçu depuis la queue: %lu\r\n", receivedValue);
-    }
-    else
-    {
-      printf("TaskTake: Timeout → reset\r\n");
-      NVIC_SystemReset();
-    }
-  }
+/* ---- printf redirection ---- */
+int __io_putchar(int ch) {
+  HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+  return ch;
 }
 
 int main(void)
@@ -52,54 +38,101 @@ int main(void)
   MX_GPIO_Init();
   MX_USART1_UART_Init();
 
-  myQueue = xQueueCreate(5, sizeof(TickType_t));
-  configASSERT(myQueue != NULL);
+  printf("System started: test reentrance & mutex\r\n");
 
-  xTaskCreate(TaskGive, "Give", 128, NULL, 2, &taskGiveHandle);
-  xTaskCreate(TaskTake, "Take", 128, NULL, 3, &taskTakeHandle);
+  /* Création du mutex */
+  printMutex = xSemaphoreCreateMutex();
+  configASSERT(printMutex != NULL);
+
+  /* Création des tâches */
+  BaseType_t ret;
+  ret = xTaskCreate(task_bug, "Tache 1", STACK_SIZE, (void *)TASK1_DELAY, TASK1_PRIORITY, &task1Handle);
+  configASSERT(ret == pdPASS);
+
+  ret = xTaskCreate(task_bug, "Tache 2", STACK_SIZE, (void *)TASK2_DELAY, TASK2_PRIORITY, &task2Handle);
+  configASSERT(ret == pdPASS);
 
   vTaskStartScheduler();
 
-  while (1) {}
+  while (1);
 }
 
+void task_bug(void *pvParameters)
+{
+  int delay = (int) pvParameters;
+
+  for (;;)
+  {
+    if (xSemaphoreTake(printMutex, portMAX_DELAY) == pdTRUE)
+    {
+      printf("Je suis %s et je m'endors pour %d ms\r\n", pcTaskGetName(NULL), delay * 1000);
+      xSemaphoreGive(printMutex);
+    }
+
+    vTaskDelay(pdMS_TO_TICKS(delay * 1000));
+  }
+}
+
+/* ---- Fonctions nécessaires à STM32CubeMX ---- */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 10;
-  RCC_OscInitStruct.PLL.PLLN = 210;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 2;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
 
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    Error_Handler();
+
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+                              | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
-  {
     Error_Handler();
-  }
+}
+
+static void MX_GPIO_Init(void)
+{
+  __HAL_RCC_GPIOI_CLK_ENABLE();
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  GPIO_InitStruct.Pin = GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOI, &GPIO_InitStruct);
 }
 
 static void MX_USART1_UART_Init(void)
 {
+  __HAL_RCC_USART1_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* PA9 = TX, PA10 = RX */
+  GPIO_InitStruct.Pin = GPIO_PIN_9 | GPIO_PIN_10;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
   huart1.Instance = USART1;
   huart1.Init.BaudRate = 115200;
   huart1.Init.WordLength = UART_WORDLENGTH_8B;
@@ -108,29 +141,11 @@ static void MX_USART1_UART_Init(void)
   huart1.Init.Mode = UART_MODE_TX_RX;
   huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
   huart1.Init.OverSampling = UART_OVERSAMPLING_16;
-  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+
   if (HAL_UART_Init(&huart1) != HAL_OK)
   {
     Error_Handler();
   }
-}
-
-int fputc(int ch, FILE *f)
-{
-  HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
-  return ch;
-}
-
-static void MX_GPIO_Init(void)
-{
-  __HAL_RCC_GPIOI_CLK_ENABLE();
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  GPIO_InitStruct.Pin = GPIO_PIN_1;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOI, &GPIO_InitStruct);
 }
 
 void Error_Handler(void)
